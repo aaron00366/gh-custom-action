bc56e7dddbee4661cfcba0700f8d24e6712423fd
Aaron <aaron@foreknowledge.com.tw>
2025-08-13 14:44:44 +0800

    added custom deploy action

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,7 +1,16 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
+// const github = require('@actions/github');
 const exec = require('@actions/exec');
 function run() {
+    // 1) Get some input values
+    const bucket = core.getInput('bucket', {required: true});
+    const bucketRegion = core.getInput('bucket-region', {required: true});
+    const distFolder = core.getInput('dist-folder', {required: true});
+
+    // 2) Upload file to S3
+    const s3Url = `s3://${bucket}/${distFolder}`;
+    exec.exec(`aws s3 sync ${distFolder} ${s3Url} --region ${bucketRegion}`);
+
     core.notice('Hello from my custom JacaScript Action!')
 }
 run();
